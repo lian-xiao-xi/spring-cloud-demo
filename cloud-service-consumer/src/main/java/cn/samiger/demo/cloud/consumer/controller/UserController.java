6056137700a79bedca98f4bb467377ff309704ca
lian-xiao-xi <13253641328@163.com>
2020-07-07 17:56:05 +0800

    feat: 使用 ribbon 负载均衡

--- cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/controller/UserController.java
+++ cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/controller/UserController.java
@@ -2,29 +2,22 @@ package cn.samiger.demo.cloud.consumer.controller;
 
 import cn.samiger.demo.cloud.consumer.pojo.CloudUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("consumer/user")
 public class UserController {
   @Autowired
-  private DiscoveryClient discoveryClient;
-  @Autowired
   private RestTemplate restTemplate;
   
+  // 使用了负载均衡
   @GetMapping("/one/{id}")
   public CloudUser allUsers(@PathVariable Long id) {
-    List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
-    ServiceInstance serviceInstance = instances.get(0);
-    String baseUrl = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/api/user/one/" + id;
+    String baseUrl = "http://service-provider/api/user/one/"+id;
     return this.restTemplate.getForObject(baseUrl, CloudUser.class);
   }
 }
