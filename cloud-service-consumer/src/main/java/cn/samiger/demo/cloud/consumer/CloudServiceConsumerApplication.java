0ba65d5147430efe70a2c32db3f0dfbdc2007ac4
lian-xiao-xi <13253641328@163.com>
2020-07-08 11:43:33 +0800

    feat: hystrix 服务降级

--- cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/CloudServiceConsumerApplication.java
+++ cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/CloudServiceConsumerApplication.java
@@ -2,10 +2,14 @@ package cn.samiger.demo.cloud.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+//@SpringCloudApplication // 组合注解
 public class CloudServiceConsumerApplication {
   
   public static void main(String[] args) {
--- cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/controller/UserController.java
+++ cloud-service-consumer/src/main/java/cn/samiger/demo/cloud/consumer/controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.samiger.demo.cloud.consumer.controller;
 
-import cn.samiger.demo.cloud.consumer.pojo.CloudUser;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +11,19 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("consumer/user")
+@DefaultProperties(defaultFallback = "fallback") // 定义类全局的熔断方法
 public class UserController {
   @Autowired
   private RestTemplate restTemplate;
   
-  // 使用了负载均衡
   @GetMapping("/one/{id}")
-  public CloudUser allUsers(@PathVariable Long id) {
+  @HystrixCommand // 标记该方法需要熔断
+  public String allUsers(@PathVariable Long id) {
     String baseUrl = "http://service-provider/api/user/one/"+id;
-    return this.restTemplate.getForObject(baseUrl, CloudUser.class);
+    return this.restTemplate.getForObject(baseUrl, String.class);
+  }
+  
+  public String fallback() {
+    return "服务正忙。。。请稍候再试";
   }
 }
