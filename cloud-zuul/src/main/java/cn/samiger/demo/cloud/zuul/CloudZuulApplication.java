f5b91a6aba9a3c162d5b2eaaa1f49867f0916507
lian-xiao-xi <13253641328@163.com>
2020-07-09 13:49:21 +0800

    feat: zuul 过滤器

--- cloud-zuul/src/main/java/cn/samiger/demo/cloud/zuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/cn/samiger/demo/cloud/zuul/CloudZuulApplication.java
@@ -2,9 +2,7 @@ package cn.samiger.demo.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
---
+++ cloud-zuul/src/main/java/cn/samiger/demo/cloud/zuul/filter/LoginFilter.java
@@ -0,0 +1,43 @@
+package cn.samiger.demo.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+  
+  @Override
+  public int filterOrder() {
+    return 100;
+  }
+  
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+  
+  @Override
+  public Object run() throws ZuulException {
+    RequestContext context = RequestContext.getCurrentContext();
+    HttpServletRequest request = context.getRequest();
+    HttpSession session = request.getSession();
+    Object token = session.getAttribute("token");
+    if(StringUtils.isEmpty(token)) {
+      context.setSendZuulResponse(false);
+      context.setResponseBody("{\"status\":\"401\", \"text\":\"request error!\"}");
+    }
+    context.set("token", token);
+    return null;
+  }
+}
